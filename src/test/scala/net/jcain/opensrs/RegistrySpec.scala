000ae9325745f734d1bf697c06f98e0d8a059825
Jim Cain <jim@jcain.net>
2016-09-28 13:35:39 -0400

    In RegistryFixture, skip the test if user and key aren't defined

--- src/test/scala/net/jcain/opensrs/RegistrySpec.scala
+++ src/test/scala/net/jcain/opensrs/RegistrySpec.scala
@@ -12,22 +12,13 @@ package net.jcain.opensrs
 
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{TestActorRef, TestKit, TestProbe}
-//import com.typesafe.config.ConfigFactory
 import org.scalatest.{Matchers, WordSpecLike}
 import concurrent.duration._
 
 object RegistrySpec {
 
-  val User = Option(System.getProperty("user")) match {
-    case Some(s) => s
-    case None => throw new RuntimeException("Please set the OpenSRS username on the command line: -Duser=myuser")
-  }
-
-  val PrivateKey = Option(System.getProperty("key")) match {
-    case Some(s) => s
-    case None => throw new RuntimeException("Please set the OpenSRS private key on the command line: -Dkey=mykey")
-  }
-
+  val User = Option(System.getProperty("user"))
+  val PrivateKey = Option(System.getProperty("key"))
   val Hostname = "horizon.opensrs.net"
 
 }
@@ -38,8 +29,13 @@ class RegistrySpec extends TestKit(ActorSystem("RegistrySpec")) with WordSpecLik
 
   class RegistryFixture(label: String, key: Option[String] = None) {
 
+    // cancel the test if user and private key are not given on command line
+    assume(User.isDefined && PrivateKey.isDefined, "Options -Duser and -Dkey are not both specified")
+
     val testProbe = TestProbe(s"$label-probe")
-    val registry = TestActorRef(Props(classOf[Registry], User, key.getOrElse(PrivateKey), Some(Hostname), None, Some(testProbe.ref)), s"$label-registry")
+    val registry = TestActorRef(Props(classOf[Registry],
+      User.get, key.getOrElse(PrivateKey.get), Some(Hostname), None, Some(testProbe.ref)
+    ), s"$label-registry")
 
     def stop() = {
       system.stop(registry)
